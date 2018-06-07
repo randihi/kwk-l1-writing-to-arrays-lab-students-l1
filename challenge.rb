404319bf77a4dedcb3e94afe3226a4e652cb88b0
randihi <randihigashi@gmail.com>
2018-06-07 16:32:11 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -1,15 +1,24 @@
-def age_order
-  sort_me = []
-  sort_me.push({"Age"=>33, "Name"=>"Frank"})
-  sort_me.push({"Age"=>15, "Name"=>"Stacy"})
-  sort_me.push({"Age"=>24, "Name"=>"Juan"})
-  sort_me.push({"Age"=>32, "Name"=>"Dom"})
-  sort_me.push({"Age"=>24, "Name"=>"Steve"})
-  sort_me.push({"Age"=>24, "Name"=>"Jill"})
+# def age_order
+#   sort_me = []
+#   sort_me.push({"Age"=>33, "Name"=>"Frank"})
+#   sort_me.push({"Age"=>15, "Name"=>"Stacy"})
+#   sort_me.push({"Age"=>24, "Name"=>"Juan"})
+#   sort_me.push({"Age"=>32, "Name"=>"Dom"})
+#   sort_me.push({"Age"=>24, "Name"=>"Steve"})
+#   sort_me.push({"Age"=>24, "Name"=>"Jill"})
 
-  sorted = sort_me.sort_by { |k| k["Age"] }
-  puts sorted[0].values
+#   sorted = sort_me.sort_by { |k| k["Age"] }
+#   puts sorted[0].values
   
-end
+# end
 
-puts age_order
+# puts age_order
+
+data = [
+  ['Frank', 33],
+  ['Stacy', 15],
+  ['Juan', 24],
+  ['Dom', 32],
+  ['Steve', 24],
+  ['Jill', 24]
+]
