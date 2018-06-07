2f81e4f9f76ba78e23550b21d0b9365321f9a847
randihi <randihigashi@gmail.com>
2018-06-07 16:30:22 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -26,7 +26,7 @@ def score
 end
 
   def letter_scores
-    score
+    scores = 
     {  "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
