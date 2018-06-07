d595eb31c627203a08f6f1284f484737916f90f2
randihi <randihigashi@gmail.com>
2018-06-07 16:04:55 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -1,16 +1,4 @@
 def age_order
-  # data = [
-  #   ["Frank", 33],
-  #   ["Stacy", 15],
-  #   ["Juan", 24],
-  #   ["Dom", 32],
-  #   ["Steve", 24],
-  #   ["Jill", 24]
-  # ]
-  
-  # data.sort_by {|name, age| age}
-  # puts data
-  
   sort_me = []
   sort_me.push({"Age"=>33, "Name"=>"Frank"})
   sort_me.push({"Age"=>15, "Name"=>"Stacy"})
@@ -20,7 +8,7 @@ def age_order
   sort_me.push({"Age"=>24, "Name"=>"Jill"})
 
   sorted = sort_me.sort_by { |k| k["Age"] }
-  puts sorted
+  puts sorted.values
   
 end
 
