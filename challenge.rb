fcb930cc958af8e8016ef9a4c28f0fcd382f62c3
randihi <randihigashi@gmail.com>
2018-06-07 16:03:22 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -15,6 +15,9 @@ def age_order
   sort_me.push({"Age"=>33, "Name"=>"Frank"})
   sort_me.push({"Age"=>15, "Name"=>"Stacy"})
   sort_me.push({"Age"=>24, "Name"=>"Juan"})
+  sort_me.push({"Age"=>32, "Name"=>"Dom"})
+  sort_me.push({"Age"=>24, "Name"=>"Steve"})
+  sort_me.push({"Age"=>24, "Name"=>"Jill"})
 
   sorted = sort_me.sort_by { |k| k["value"] }
   puts sorted
